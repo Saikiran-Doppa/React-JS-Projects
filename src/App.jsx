f39bb455bab55acbfb696365e2b984322763409b
saikiran <saikiran.doppa@gmail.com>
2025-05-27 11:09:10 +0530

    normal commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
-<<<<<<< HEAD
 import WelcomeMsg from "./Components/WelcomeMsg";
 import { useState } from "react";
 
@@ -28,29 +27,6 @@ function App() {
         todoItems={todoItems}
         handleDeleteItem={onDeleteItem}
       ></TodoItems>
-=======
-
-function App() {
-  let todoItems = [
-    {
-      name: "Finish React-JS",
-      dueDate: "31-May-2025",
-    },
-    {
-      name: "Revision",
-      dueDate: "31-May-2025",
-    },
-    {
-      name: "Dinner",
-      dueDate: "In 1 hour",
-    },
-  ];
-  return (
-    <center>
-      <AppName></AppName>
-      <AddTodo></AddTodo>
-      <TodoItems todoItems={todoItems}></TodoItems>
->>>>>>> 9ffac0a672fdebaee513369b9d9b016b9e7748a1
     </center>
   );
 }
