b5f6deafe41c6efc7736bc57825c27d3b1725f77
saikiran <saikiran.doppa@gmail.com>
2025-05-30 17:36:07 +0530

    UI Define

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Display from "./Components/Display";
+import ButtonContainer from "./Components/ButtonContainer";
+
+function App() {
+  return (
+    <div className="mainContainer">
+      <Display></Display>
+      <ButtonContainer></ButtonContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Display.jsx
@@ -0,0 +1,7 @@
+import styles from "./Display.module.css";
+
+const Display = () => {
+  return <input type="text" readOnly className={styles.inputDisplay} />;
+};
+
+export default Display;
