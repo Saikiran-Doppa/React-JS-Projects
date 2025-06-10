79b9dbe604016f9486794de798542d1d41d2cbeb
saikiran <saikiran.doppa@gmail.com>
2025-06-10 18:40:14 +0530

    Revised

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -2,33 +2,19 @@ import "./App.css";
 import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
-import { useState } from "react";
 import WelcomeMsg from "./Components/WelcomeMsg";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  const [todoItems, setTodoitems] = useState([]);
-
-  const addNewItem = (itemName, itemDate) => {
-    setTodoitems((currValue) => [
-      ...currValue,
-      { name: itemName, dueDate: itemDate },
-    ]);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const remItem = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoitems(remItem);
-  };
   return (
-    <TodoItemsContext.Provider value={{ todoItems, addNewItem, deleteItem }}>
+    <TodoItemsContextProvider>
       <center>
         <AppName></AppName>
         <AddTodo></AddTodo>
         <WelcomeMsg></WelcomeMsg>
         <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
